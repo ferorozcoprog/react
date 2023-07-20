4f24573ee6b6e73ac3f5ea58fed0e3b9cf1d18f5
fernando <fernandoorozcoprog@gmail.com>
2023-07-19 23:56:17 -0300

    segunda entrega orozco

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,40 @@
 import { Header } from "./components/Header/Header"
-import  {ItemListContainer } from "./components/ItemListContainer/ItemListContainer"
 import "./styles/styles.scss"
+import "./App.css"
+import { ProductCard } from "./components/ProductCard/ProductCard"
 
 function App() {
   
-  return(
-<div>
-
-<Header />
-
-<ItemListContainer notificacion= {"Próximamente estarán los articulos a la venta"} />
-
-</div>
-
-  ) 
-}
-
-export default App
+  const products = [
+        {
+          name: "Camisa 1",
+          price: 129.99,
+          imageUrl: "/Camisa1.jpg",
+        },
+        {
+          name: 'Camisa 2',
+          price: 109.99,
+          imageUrl: "/Camisa2.jpg",
+        },
+    
+      ]
+    
+      return (
+        <div>
+          <Header />
+          <div className="flex justify-center items-center min-h-screen bg-gray-100">
+          <div className="grid gap-4 grid-cols-3">
+            {products.map((product, index) => (
+              <ProductCard
+                key={index}
+                name={product.name}
+                price={product.price}
+                imageUrl={product.imageUrl}
+              />
+            ))}
+          </div>
+        </div>
+        </div>
+      )
+    }
+    export default App
